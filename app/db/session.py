f75132fe9be35654e48ed600073d71d7a661bb1a
AndrewwSS <clash2clans1one@gmail.com>
2024-10-19 16:10:40 +0300

    update to async engine

--- app/db/session.py
+++ app/db/session.py
@@ -1,16 +1,16 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
 SQLALCHEMY_DATABASE_URL = settings.database_url
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 
 
-def get_db():
+async def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
-        db.close()
+        await db.close()
