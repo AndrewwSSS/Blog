06e1fbb8aab3536a6187435ac64fbea21ebe8a6c
AndrewwSS <clash2clans1one@gmail.com>
2024-10-21 14:55:33 +0300

    update current user retrieving dependency

--- app/core/auth/dependencies.py
+++ app/core/auth/dependencies.py
@@ -1,6 +1,7 @@
 import jwt
 from fastapi.security import OAuth2PasswordBearer
 from fastapi import Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.auth.utils import verify_token
 from app.db.session import get_session
@@ -9,11 +10,11 @@ from app.repositories.user_repository import UserRepository
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(token: str = Depends(oauth2_scheme), session: AsyncSession = Depends(get_session)):
     try:
         user_id = verify_token(token)
-        repository = UserRepository(get_session())
-        user = repository.get_user_by_id(user_id)
+        repository = UserRepository(session)
+        user = await repository.get_user_by_id(user_id)
         if not user:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
