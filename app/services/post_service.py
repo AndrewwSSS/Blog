c84364553f2c75b5ccc3a3997b842df1fc524914
AndrewwSS <clash2clans1one@gmail.com>
2024-10-21 16:50:30 +0300

    add post and comment services

--- app/services/post_service.py
+++ app/services/post_service.py
@@ -1,6 +1,16 @@
 from app.repositories.post_repository import PostRepository
+from app.schemas.post import Post
+from app.schemas.post import PostRead
+from app.schemas.user import UserRead
 
 
 class PostService:
     def __init__(self, post_repository: PostRepository) -> None:
         self.repository = post_repository
+
+    async def get_posts(self) -> [PostRead]:
+        return await self.repository.get_posts()
+
+    async def create_post(self, post: Post, user: UserRead) -> PostRead:
+        return await self.repository.create_post(post, user)
+
