fbb01226b8d1d7fd722691718da7290f3224919b
AndrewwSS <clash2clans1one@gmail.com>
2024-10-21 14:53:16 +0300

    fix typo and type checks

--- app/core/auth/utils.py
+++ app/core/auth/utils.py
@@ -19,7 +19,7 @@ def create_refresh_token(data: dict, expires_delta: timedelta = None):
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(days=settings.REFRESH_TOKEN_DAYS)
+        expire = datetime.utcnow() + timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
@@ -29,7 +29,7 @@ def verify_token(token: str) -> int:
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         user_id: str = payload.get("user_id")
-        if not user_id or not user_id.isdigit():
+        if not user_id or not isinstance(user_id, int):
             raise jwt.InvalidTokenError("Invalid token")
         return int(user_id)
     except jwt.ExpiredSignatureError:
