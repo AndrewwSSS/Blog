441c62f94acef4a1b3fec0837a2de5fcb2df8f6f
AndrewwSS <clash2clans1one@gmail.com>
2024-10-21 14:56:24 +0300

    add endpoints to obtain and refresh tokens

--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -2,6 +2,7 @@ from typing import List
 
 from fastapi import APIRouter, Depends
 
+from app.core.auth.dependencies import get_current_user
 from app.dependencies.services import get_user_service
 from app.schemas.user import User, UserRead
 from app.services.user_service import UserService
@@ -19,20 +20,25 @@ async def create_user_endpoint(
 
 @router.get("/", response_model=List[UserRead])
 async def get_users_endpoint(
+    user: User = Depends(get_current_user),
     service: UserService = Depends(get_user_service)
 ) -> [UserRead]:
     return await service.read_users()
 
 
-@router.get("/token", response_model=List[UserRead])
-async def get_users_endpoint(
+@router.post("/token")
+async def login(
+    user: User,
     service: UserService = Depends(get_user_service)
-) -> [UserRead]:
-    return await service.read_users()
+):
+    return await service.get_jwt_tokens(user)
 
 
-@router.get("/token/refresh", response_model=List[UserRead])
-async def get_users_endpoint(
+@router.post("/token/refresh")
+async def refresh_access_token(
+    refresh_token: str,
     service: UserService = Depends(get_user_service)
-) -> [UserRead]:
-    return await service.read_users()
+):
+    return await service.refresh_access_token(refresh_token)
+
+
--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,8 +1,10 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.models import User
+from app.core.security import get_password_hash
+from app.models import UserDB
 from app.schemas.user import User
+from app.schemas.user import UserInDB
 from app.schemas.user import UserRead
 
 
@@ -14,9 +16,10 @@ class UserRepository:
         self,
         user: User,
     ) -> UserRead:
-        user_db = User(
+
+        user_db = UserDB(
             username=user.username,
-            hashed_password=user.password,
+            hashed_password=get_password_hash(user.password),
             email=user.email,
         )
         self.session.add(user_db)
@@ -28,19 +31,36 @@ class UserRepository:
         )
 
     async def get_users(self) -> [UserRead]:
-        query = select(User)
+        query = select(UserDB)
         cities_list = await self.session.execute(query)
         return [
             UserRead.model_validate(city[0])
             for city in cities_list.fetchall()
         ]
 
-    async def get_user_by_id(self, user_id: int) -> UserRead:
-        query = select(User).where(User.id == user_id)
+    async def get_user_by_id(self, user_id: int) -> UserRead | None:
+        query = select(UserDB).where(UserDB.id == user_id)
         result = await self.session.execute(query)
         user_db = result.scalar_one_or_none()
         if user_db:
             return UserRead.model_validate(
                 user_db
             )
+
+    async def get_user_by_username(self, username: str) -> UserInDB | None:
+        query = select(UserDB).where(UserDB.username == username)
+        result = await self.session.execute(query)
+        user_db = result.scalar_one_or_none()
+        if user_db:
+            return UserInDB.model_validate(
+                user_db
+            )
+
+    async def get_user_hashed_password(self, user_id: int) -> str | None:
+        query = select(UserDB).where(UserDB.id == user_id)
+        result = await self.session.execute(query)
+        user_db = result.scalar_one_or_none()
+        if user_db:
+            return user_db.hashed_password
+
 
