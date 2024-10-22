31011f0f02a420ef5fc9041a0115cdb619de8db3
AndrewwSSS <clash2clans1one@gmail.com>
2024-10-22 19:46:20 +0300

    add comment-daily-breakdown endpoint

--- app/api/v1/endpoints/comments.py
+++ app/api/v1/endpoints/comments.py
@@ -1,9 +1,11 @@
+from datetime import date
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 
 from app.core.auth.dependencies import get_current_user
 from app.dependencies.services import get_comment_service
+from app.schemas.analytics import CommentAnalytic, CommentsAnalyticsRequest
 from app.schemas.comment import Comment
 from app.schemas.comment import CommentRead
 from app.schemas.user import UserRead
@@ -29,3 +31,15 @@ async def get_posts(
     service: CommentService = Depends(get_comment_service)
 ):
     return await service.get_comments()
+
+
+@router.get("/comments-daily-breakdown", response_model=List[CommentAnalytic])
+async def get_comments_daily_breakdown(
+    date_from: date = Query(..., description="Start date for the analytics in YYYY-MM-DD format"),
+    date_to: date = Query(..., description="End date for the analytics in YYYY-MM-DD format"),
+    user: UserRead = Depends(get_current_user),
+    service: CommentService = Depends(get_comment_service)
+):
+    return await service.get_comments_analytics(
+        date_from, date_to
+    )
