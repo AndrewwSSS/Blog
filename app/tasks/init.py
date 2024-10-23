121056e9e6b8e425d69b2ac1fb01cf6ea614c56c
AndrewwSS <clash2clans1one@gmail.com>
2024-10-23 14:25:56 +0300

    reformat code style

--- app/tasks/init.py
+++ app/tasks/init.py
@@ -20,35 +20,36 @@ def create_reply_for_post(post_id: int):
     asyncio.run(_create_reply_for_post(post_id))
 
 
-async def _create_reply_for_post(post_id: int):
+async def _create_reply_for_post(post_id: int) -> None:
     # get valid session
-    session = get_session()
+    session_gen = get_session()
+    session = await anext(session_gen)
+
     # get post
     post_repo = PostRepository(session)
     post = await post_repo.get_post_by_id(post_id)
     if not post:
         raise ValueError("Post not found")
 
-    # generate reply
     reply_generator = settings.REPLY_GENERATOR_CLASS()
     reply_content = await reply_generator.generate_post_reply(
         post.title, post.content,
     )
     user_repo = UserRepository(session)
     bot = await user_repo.get_user_by_username(
-        settings.BOT_USERNAME
+        settings.REPLY_BOT_USERNAME
     )
     if not bot:
         raise ValueError("Bot not found")
 
-    # create reply comment
     comment_repo = CommentRepository(session)
     comment = Comment(
         content=reply_content,
         owner_id=bot.id,
         post_id=post_id,
     )
-    created_comment = await comment_repo.create_comment(comment)
-    print(f"{created_comment=}")
-
-
+    await comment_repo.create_comment(
+        comment,
+        bot,
+        settings.CONTENT_VALIDATOR_CLASS,
+    )
